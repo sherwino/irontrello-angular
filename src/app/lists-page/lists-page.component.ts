e78bba8d69dfab683f821cc58541b47b002a113f
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-07-15 18:09:34 -0400

    Add list of cards

--- src/app/lists-page/lists-page.component.ts
+++ src/app/lists-page/lists-page.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ListService } from '../services/list.service';
+
 @Component({
   selector: 'app-lists-page',
   templateUrl: './lists-page.component.html',
   styleUrls: ['./lists-page.component.css']
 })
 export class ListsPageComponent implements OnInit {
+  myLists: any[] = [];
 
-  constructor() { }
+  constructor(
+    private listThang: ListService
+  ) { }
 
   ngOnInit() {
+      this.listThang.lists()
+        .then((listsFromApi) => {
+            this.myLists = listsFromApi;
+        })
+        .catch((errResponse) => {
+            alert('List error 🐋');
+        });
   }
 
 }
--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class ListService {
+  baseUrl: string = 'http://localhost:3000';
 
-  constructor() { }
+  constructor(
+    private httpThang: Http
+  ) { }
 
+  lists() {
+      return this.httpThang
+        .get(
+          this.baseUrl + '/api/lists',
+          { withCredentials: true }
+        )
+        .toPromise()
+        .then(res => res.json());
+  }
 }
